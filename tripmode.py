00f982cd5f2d9e13fe0cf4d8a9692bb2607dc812
Sean LaPlante <laplante.sean@gmail.com>
2021-11-14 15:46:46 -0500

    GUI Improvements

--- tripmode.py
+++ tripmode.py
@@ -1,18 +1,14 @@
+#!/usr/bin/python
 """Chevy Volt trip mode."""
-import sys
+import os
 import time
 import struct
-import logging
-import threading
 from typing import List, Union
 
 from panda import Panda
 import PySimpleGUI as sg
 
-# Setup basic logging
-logging.basicConfig(handlers=[
-    logging.FileHandler("tripmode.log"),
-    logging.StreamHandler(stream=sys.stdout)])
+DEFAULT_SPEED_THRESHOLD = 50
 
 
 class CarState:
@@ -33,13 +29,25 @@ class CarState:
 
     def __init__(self, p: Panda):
         """Set up the car state."""
-        self.speed_threshold = 50
+        self.speed_threshold = DEFAULT_SPEED_THRESHOLD
         self.speed = 0
         self.panda = p
         self.allow_mode_switch_after = time.perf_counter() + self.MODE_SWITCH_COOLDOWN
         self.allow_button_press_after = 0
         self.mode = "NORMAL"
         self.pending_sends: List[List[List[Union[bytearray, int, None]]]] = []
+
+    def increase_speed_threshold(self, amnt: int = 5):
+        """Increase the speed threshold."""
+        self.speed_threshold += amnt
+        if self.speed_threshold > 70:
+            self.speed_threshold = 70
+
+    def decrease_speed_threshold(self, amnt: int = 5):
+        """Decrease the speed threshold."""
+        self.speed_threshold -= amnt
+        if self.speed_threshold < 40:
+            self.speed_threshold = 40
 
     def __del__(self):
         self.close()
@@ -50,7 +58,7 @@ class CarState:
         if now <= self.allow_mode_switch_after:
             return
 
-        logging.info(f"Switch to {new_mode} mode. Speed: {self.speed}")
+        print(f"Switch to {new_mode} mode. Speed: {self.speed}")
 
         # Update our cooldown and mode
         self.allow_mode_switch_after = now + self.MODE_SWITCH_COOLDOWN
@@ -59,7 +67,6 @@ class CarState:
         # Required presses starts at 1 (to activate the screen) and
         # mode selection always starts on NORMAL.
         required_presses = 1 + self.DRIVE_MODES.index(new_mode)
-        logging.debug(f"Needs {required_presses} presses")
         for _ in range(required_presses):
             cluster = []
             for _inner in range(self.SEND_CLUSTER_SIZE):
@@ -76,10 +83,8 @@ class CarState:
 
         self.speed = speed
         if self.speed > self.speed_threshold and self.mode == "NORMAL":
-            logging.debug(f"Speed trigger (attempt HOLD): {self.speed}")
             self._switch_modes("HOLD")
         elif self.speed <= self.speed_threshold and self.mode == "HOLD":
-            logging.debug(f"Speed trigger (attempt NORMAL): {self.speed}")
             self._switch_modes("NORMAL")
 
     def update(self):
@@ -97,19 +102,23 @@ class CarState:
                 self.panda.can_send_many(send)
 
     def close(self):
-        self.panda.close()
+        if self.panda:
+            self.panda.close()
+            self.panda = None
 
 
 def enable() -> CarState:
     """Enable trip mode."""
+    os.system("sudo udevadm trigger")
+
     try:
         p = Panda()
         p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)  # Turn off all safety preventing sends
         p.set_can_enable(0, True)  # Enable bus 0 for output
         p.can_clear(0xFFFF)  # Flush the panda CAN buffers
     except Exception as exc:
-        logging.error(f"Failed to connect to Panda! {exc}")
-        return
+        print(f"Failed to connect to Panda: {exc}")
+        return None
 
     return CarState(p)
 
@@ -122,35 +131,67 @@ def main() -> None:
     # Theme and layout for the window
     sg.theme('DarkAmber')
     layout = [
-        [sg.Text('TRIP MODE')],
-        [sg.Button('ON'), sg.Button('OFF')]
+        [sg.Button('/\\', font=("Times New Roman", 96)), sg.Text('TRIP MODE', font=("Times New Roman", 66))],
+        [sg.Text(size=(2, 1), key='-THRESH-', font=("Times New Roman", 96)), sg.Button('ON', font=("Times New Roman", 96)), sg.Button('OFF', font=("Times New Roman", 96))],
+        [sg.Button('\\/', font=("Times New Roman", 96)), sg.Output(size=(40, 10), font=('Consolas 11')), sg.Button('Exit', font=("Times New Roman", 32))]
     ]
 
     # Create the Window (800x480 is Raspberry Pi touchscreen resolution)
     window = sg.Window(
         'Trip Mode', layout, finalize=True,
         keep_on_top=True, no_titlebar=True,
-        location=(0, 0), size=(800, 480))
+        location=(0, 0), size=(800, 480),
+        element_justification='c')
     window.maximize()  # Make it fullscreen
+    window['OFF'].update(disabled=True)  # Start with the off button disabled
+    window['/\\'].update(disabled=True)
+    window['\\/'].update(disabled=True)
 
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, _values = window.read(timeout=0)  # Return immediately
-        if event == sg.WIN_CLOSED:
+        if event == sg.WIN_CLOSED or event == "Exit":
+            print("Exiting...")
             if car_state:
                 car_state.close()
             break
 
+        # Disable both buttons if either of them are pressed
+        if event == "ON" or event == "OFF":
+            window['ON'].update(disabled=True)
+            window['OFF'].update(disabled=True)
+            window['/\\'].update(disabled=True)
+            window['\\/'].update(disabled=True)
+
+        # Then perform the action and re-enable the
+        # correct action.
         if event == "ON" and not trip_mode_enabled:
-            trip_mode_enabled = True
             car_state = enable()
-        elif event == "OFF" and trip_mode_enabled:
+            if car_state:
+                trip_mode_enabled = True
+                window['OFF'].update(disabled=False)
+                window['/\\'].update(disabled=False)
+                window['\\/'].update(disabled=False)
+                print("Trip mode enabled!")
+
+        if event == "OFF" and trip_mode_enabled:
             trip_mode_enabled = False
             car_state.close()
             car_state = None
+            window['ON'].update(disabled=False)
+            print("Trip mode disabled!")
+
+        if event == "/\\" and car_state:
+            car_state.increase_speed_threshold()
+
+        if event == "\\/" and car_state:
+            car_state.decrease_speed_threshold()
 
         if car_state:
             car_state.update()
+            window['-THRESH-'].update(str(car_state.speed_threshold))
+        else:
+            window['-THRESH-'].update(str(DEFAULT_SPEED_THRESHOLD))
 
     window.close()
 
